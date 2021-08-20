9165bf814f28d1d343611ac60a9833271a02c55c
Ania Walker <ania.wlkr@gmail.com>
2021-08-21 01:05:03 +0300

    initial commit

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1>Wallet</h1>
+    </div>
+  );
+}
+
+export default App;
