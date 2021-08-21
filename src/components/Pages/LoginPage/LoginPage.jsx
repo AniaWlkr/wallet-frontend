6464a4ba6265b6a54affa834404aebcff1ef1a45
Elena Gubarenko <lenagritsishina@gmail.com>
2021-08-21 12:07:13 +0300

    adding changes

---
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import routes from '../../../routes/routes';
+import LoginForm from '../../LoginForm';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <p>LoginPage</p>
+      <div>
+        <NavLink to={routes.homepage}>Home</NavLink>
+      </div>
+      <LoginForm />
+    </div>
+  );
+}
---
+++ src/routes/routes.js
@@ -0,0 +1,8 @@
+const routes = {
+  homepage: '/',
+  register: '/register',
+  login: '/login',
+  error: '/404',
+};
+
+export default routes;
