32bf1dad96056eb6eef7a0c00c11e4f382dbe094
Katerina Kucheriavenko <ekucheryavenko0925@gmail.com>
2021-08-25 19:43:06 +0300

    adds a subscription to the state of the Redux store

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,12 +1,17 @@
+import { useSelector } from 'react-redux';
+import transSelectors from '../../redux/transactions/transSelectors.js';
 import style from './Balance.module.scss';
 
 export default function Balance() {
+  const total = useSelector(transSelectors.getBalance);
+
   return (
     <div className={style.balance}>
       <p className={style.balanceTitle}>Ваш баланс</p>
       <p className={style.balanceValue}>
         <span className={style.symbol}>₴</span>
-        24 000.00
+        {/* 24 000.00 */}
+        {total}
       </p>
     </div>
   );
