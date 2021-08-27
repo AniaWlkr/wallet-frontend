77cc9a990f776ab0afc36ba9d562be046f390f4f
Ania Walker <ania.wlkr@gmail.com>
2021-08-27 11:42:34 +0300

    slight corrections

--- src/components/Pages/DashboardPage/DashboardPage.jsx
+++ src/components/Pages/DashboardPage/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
 import HomeTab from '../../HomeTab';
