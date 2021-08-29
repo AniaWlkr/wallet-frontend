6a09a5d3ac2987d38a572b746f9c1f778962c9dd
Katerina Kucheriavenko <ekucheryavenko0925@gmail.com>
2021-08-29 04:22:20 +0300

    fixes styles and throws props to the child elements

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,17 +1,18 @@
 import { useSelector } from 'react-redux';
-import transSelectors from '../../redux/transactions/transSelectors.js';
+import financeSelectors from '../../redux/finance/financeSelectors';
+import { normalizedSum } from '../../utils/normalizedSum';
 import style from './Balance.module.scss';
 
 export default function Balance() {
-  const total = useSelector(transSelectors.getBalance);
+  const total = useSelector(financeSelectors.getCurrentUserBalance);
+  const currentBalance = normalizedSum(total);
 
   return (
     <div className={style.balance}>
       <p className={style.balanceTitle}>Ваш баланс</p>
       <p className={style.balanceValue}>
         <span className={style.symbol}>₴</span>
-        {/* 24 000.00 */}
-        {total}
+        {currentBalance}
       </p>
     </div>
   );
